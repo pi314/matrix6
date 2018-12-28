97ef4eb639ff9ed196add247f4df38ed414b96ed
Cychih <michael66230@gmail.com>
2018-12-29 01:54:56 +0800

    Add gold flow and easter eggs

--- matrix6.js
+++ matrix6.js
@@ -13,23 +13,41 @@ var screen = {
 };
 
 
+var easter_egg = {
+    year: 0,
+    month: 0,
+    day: 0,
+    trigger: false,
+};
+
+
 function flow (col, head, len, visible) {
     this.visible = visible;
+    this.gold = this.visible ? rand_gold() : false;
     this.col = col;
     this.head = head;
     this.tail = head - len;
 
+    if (easter_egg.trigger) {
+        this.gold = true;
+        easter_egg.trigger = false;
+    }
+
     this.move = function () {
         if (this.visible && 0 <= this.head && this.head < screen.height) {
-            screen.cell[this.head][this.col].text(rand_char());
             screen.cell[this.head][this.col].removeClass('black bright dark head');
             screen.cell[this.head][this.col].addClass(rand_bright() ? 'bright' : 'dark');
         }
         this.head++;
         if (this.visible && 0 <= this.head && this.head < screen.height) {
-            screen.cell[this.head][this.col].text(rand_char());
-            screen.cell[this.head][this.col].removeClass('black bright dark');
+            screen.cell[this.head][this.col].removeClass('black bright dark head gold');
             screen.cell[this.head][this.col].addClass('head');
+            if (this.gold) {
+                screen.cell[this.head][this.col].addClass('gold');
+                screen.cell[this.head][this.col].text(rand_char(this.head));
+            } else {
+                screen.cell[this.head][this.col].text(rand_char());
+            }
         }
 
         if (this.visible && 0 <= this.tail && this.tail < screen.height) {
@@ -82,7 +100,9 @@ $(function init () {
     }, frame_delay_min);
 
     $(window).keydown(function (event) {
-        if (event.which == 32) {
+        if (event.which == 13) {
+            easter_egg.trigger = true;
+        } else if (event.which == 32) {
             pause = !pause;
             if (!pause && paused) {
                 setTimeout(function () {
@@ -94,9 +114,23 @@ $(function init () {
 });
 
 
-function rand_char () {
+function rand_char (row) {
     var sample_space = '1234567890-=!@#$%^&*()_+qwertyuiop[]QWERTYUIOP{}asdfghjkl;ASDFGHJKL:zxcvbnm/ZXCVBNM<>?';
-    return sample_space[Math.floor(Math.random() * sample_space.length)];
+    var idx = Math.floor(Math.random() * sample_space.length);
+
+    if (row != undefined) {
+        if (easter_egg.month == 12 && easter_egg.day == 25) {
+            sample_space = 'MerryChristmas!!!';
+            idx = row % sample_space.length;
+        }
+
+        if ((easter_egg.month == 11 && easter_egg.day >= 29) || (easter_egg.month == 0 && easter_egg.day <= 04)) {
+            sample_space = 'HappyNewYear' + (easter_egg.year + (easter_egg.month != 0)) + '!!!';
+            idx = row % sample_space.length;
+        }
+    }
+
+    return sample_space[idx];
 }
 
 
@@ -112,6 +146,11 @@ function rand_head () {
 
 function rand_len () {
     return Math.floor(Math.random() * (screen.height * 4 / 5) + screen.height / 8);
+}
+
+
+function rand_gold () {
+    return (Math.floor(Math.random() * 300) == 0) ? true : false;
 }
 
 
@@ -139,4 +178,9 @@ function next_frame () {
             next_frame();
         }, frame_delay_min);
     }
+
+    var today = new Date();
+    easter_egg.year = today.getFullYear();
+    easter_egg.month = today.getUTCMonth();
+    easter_egg.day = today.getDate();
 }
